fa93a53907ebadff9e270b3b0856359f991d4365
syed <syedmatheen55@gmail.com>
2020-09-22 20:45:06 +0530

    Add files via upload

---
+++ ImmutableClass.java
@@ -0,0 +1,9 @@
+
+public class ImmutableClass {
+	public static void main(String args[]){
+		Student student = new Student("Rajesh",1000);
+		System.out.println("1 "+student.getName());
+		System.out.println("2 "+student.getregNo());
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+public final class Student {
+	final String name;
+	final int regNo;
+
+	public Student(String name, int regNo) {
+		this.name = name;
+		this.regNo = regNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getregNo() {
+		return regNo;
+	}
+
+}
